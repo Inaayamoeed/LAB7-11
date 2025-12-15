f2b434a9945f34ea792b0d1f3be3e0d7b315e76a
Inaayamoeed <IM697@live.mdx.ac.uk>
2025-12-15 23:28:53 +0400

    Delete Week11 directory

--- Week11/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from models.user import User
-from models.security_incident import SecurityIncident
-from models.dataset import Dataset
-from models.it_ticket import ITTicket
-
-_all_ = ["User", "SecurityIncident", "Dataset", "ITTicket"]
--- Week11/services/_init_.py
+++
@@ -1,5 +0,0 @@
-from services.database_manager import DatabaseManager
-from services.auth_manager import AuthManager
-from services.ai_assistant import AIAssistant
-
-__all__ = ["DatabaseManager", "AuthManager", "AIAssistant"]
